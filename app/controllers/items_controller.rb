7d1d4be9450bbbfd6b38baa180fb915defea16e2
shuto-shinoda <shuto.88.tb@gmail.com>
2020-09-08 16:12:25 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
   def index
+  def message_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   has_many :comments, dependent::destroy
   belongs_to :user
   has_one :purchases
+  has_one_attached :image
 end
