9471717194adc42afdbcb3f368dfc7e77ddf4307
shuto-shinoda <shuto.88.tb@gmail.com>
2020-09-08 16:22:38 +0900

    入力必須

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchases
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+    validates :price
+    validates :item_condition
+    validates :postage_payer
+    validates :prefecture_code
+    validates :preparation_day
+    validates :category
+  end
 end
