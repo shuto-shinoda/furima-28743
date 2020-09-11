d5ee2f08b1dfc2d93c0785e0a9896b75a96beb5b
shuto-shinoda <shuto.88.tb@gmail.com>
2020-09-11 13:15:25 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Name.name}
+    introduction {Faker::Lorem.sentence}
+    association :user
+    price           {1111}
+    condition_id       {2}
+    postage_payer_id   {2}
+    prefecture_code_id {2}
+    preparation_day_id {2}
+    category_id        {2}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/flag.png'), filename: 'test_image.png')
+    end
   end
 end
