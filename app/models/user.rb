a3bf5e629626598de8a5b7f38cccab0cbd02185b
shuto-shinoda <shuto.88.tb@gmail.com>
2020-09-17 18:30:39 +0900

    ＃

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   
   with_options presence: true do
     validates :name
-    PASSWORD_REGEX = /\A(?=.6?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    #PASSWORD_REGEX = /\A(?=.6?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :second_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
